86bbb7330fc9e9841a6ffed654a603da146359b0
Mtlukasik <mtlukasik@gmail.com>
2023-09-20 22:14:19 +0300

    Create data_generation_config.py

---
+++ data_generation_config.py
@@ -0,0 +1,10 @@
+# Constants for data generation
+N_SAMPLES = 10000000
+N_TARGET_STORE_IDS = 10
+N_CREATIVE_PACK_IDS = 100
+
+# Paths for saving datasets
+DRIVE_PATH = "/content/drive/My Drive/"
+PARQUET_FILENAME = "dataset_v{}.parquet"
+TFRECORD_FILENAME = "dataset_v{}.tfrecord"
+VERSION = 1
